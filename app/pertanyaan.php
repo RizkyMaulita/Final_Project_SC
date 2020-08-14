3a907f9393a1bc6ed5fa74ba2b55bf5625588347
ythyayat <ythyayathidayat2@gmail.com>
2020-08-14 07:41:54 +0700

    nambahin orm relasi antara user, pertanyaan, dan jawaban

--- app/pertanyaan.php
+++ app/pertanyaan.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class pertanyaan extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function jawaban()
+    {
+        return $this->hasMany('App\Jawaban');
+    }
+
+    public function j_tepat()
+    {
+        return $this->belongsTo('App\Jawaban','jawaban_tepat_id');
+    }
 }
