8ebbdf7b6a48054e0d73d7e209d4c8d50a8f29da
RizkyMaulita <rizky.maulita@gmail.com>
2020-08-14 23:00:03 +0700

    show

--- resources/views/pertanyaans/show.blade.php
+++ resources/views/pertanyaans/show.blade.php
@@ -1,20 +1,76 @@
 @extends('adminlte.master')
 
 @section('content')
-<div class="mt-3 ml-3">
-    <h4> {{ $questions -> judul}} </h4>
-    <p> {!! $questions -> isi !!} </p>
-    <p> Author : {{ $questions -> user -> name }} </p>
-
-    <div>
-        Tags: 
-        @forelse($questions -> tag as $t)
-            <button class='btn btn-primary btn-sm'> {{ $t -> tag_name }} </button>
-
-            @empty 
-            No Tags
-        @endforelse
+    <div class="container-fluid mt-3">
+        <div class="row">
+            <div class="col-md-9">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title"> {{ $questions -> judul }} </h3>
+                    </div>
+                    <div class="card-body">
+                        <p> {!! $questions -> isi !!} </p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title"> Jawaban </h3>
+                    </div>
+                    <div class="card-body">
+                        <p> isi Jawaban </p>
+                    </div>
+                </div>  
+            </div>
+          
+            <div class="col-md-3">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Tags</h3>
+                        <div class="card-tools">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i> </button>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        @forelse($questions -> tag as $t)
+                            <button class='btn btn-outline-secondary btn-sm'> {{ $t -> tag_name }} </button>
+                            @empty 
+                            No Tags
+                        @endforelse 
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title"> Komentar </h3>
+                        <div class="card-tools">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i> </button>
+                        </div>
+                    </div>
+                    <div class="card-body"> 
+                        <p> Isi Komentar </p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title"> Informasi </h3>
+                        <div class="card-tools">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i> </button>
+                        </div>
+                    </div>
+                    <div class="card-body"> 
+                        <dl class="row">
+                        <dt class = "col-sm-6"> Nama Pembuat: </dt>
+                        <dd class="col-sm-6"> {{$questions -> user -> name}} </dd>
+                        <dt class=" col-sm-6"> Tanggal dibuat: </dt>
+                        <dd class="col-sm-6"> {{$questions -> created_at}}</dd>
+                        <dt class="col-sm-6"> Tanggal diupdate: </dt>
+                        <dd class="col-sm-6"> {{$questions -> updated_at}}</dd>
+                        </dl>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
-  
+
+
+
 @endsection
