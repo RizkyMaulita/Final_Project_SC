46ce5683a17ae4276206bf3fa21370b171c3b465
ythyayat <ythyayathidayat2@gmail.com>
2020-08-14 09:38:15 +0700

    Memasangkan Auth pada project

--- app/tag.php
+++ app/tag.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class tag extends Model
 {
-    //
+    protected $guarded = [];
+
+
+    public function pertanyaan()
+    {
+        return $this->belongsToMany('App\pertanyaan','pertanyaantags','user_id','pertanyaan_id');
+    }
 }
