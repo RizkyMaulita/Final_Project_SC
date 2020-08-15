6239424b69c690af65056581312137f915ef17ba
RizkyMaulita <rizky.maulita@gmail.com>
2020-08-15 22:46:34 +0700

    kita stack

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <!-- <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
 
@@ -17,14 +17,14 @@
                     You are logged in!
                 </div>
             </div>
-        </div>
+        </div> -->
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header"> Kita Stuck </div>
+                <div class="card-header"> Kamu stack dalam mengerjakan coding ? </div>
 
                 <div class="card-body">
-                    Yuk Masuk ke Forum 'Kita Stuck'
-                    <a href="/pertanyaans" class="btn btn-primary btn-sm m-1"> Forum </a>
+                    <p> Sama !! Daripada kita bingung karena stack, lebih baik masuk yuk ke Forum 'Kita Stack' </p>
+                    <a href="/pertanyaans" class="btn btn-primary btn-sm m-1"> Forum Kita Stack </a>
                 </div>
             </div>
         </div>
